82fb10f7567571c6c5d63c45c518b750a321c03d
Sponscar <panji.sabilillah23@student.uisi.ac.id>
2026-01-17 01:54:03 +0700

    Membuat data user entities

---
+++ lib/domain/entities/user.dart
@@ -0,0 +1,15 @@
+class UserEntity {
+  final String uid;
+  final String name;
+  final String email;
+  final String phoneNumber;
+  final String role;
+
+  UserEntity({
+    required this.uid,
+    required this.name,
+    required this.email,
+    required this.phoneNumber,
+    required this.role,
+  });
+}
