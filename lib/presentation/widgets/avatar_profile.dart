7df0a44db31ad970b10378ea9c5d806b9f066d5e
Sponscar <panji.sabilillah23@student.uisi.ac.id>
2026-01-18 03:08:22 +0700

    Update avatar profile supaya menggunakan photoBase64

--- lib/presentation/widgets/avatar_profile.dart
+++ lib/presentation/widgets/avatar_profile.dart
@@ -1,18 +1,37 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 
 class ProfileAvatar extends StatelessWidget {
   final String? photoPath;
+  final String? photoBase64;
   final VoidCallback onTap;
 
   const ProfileAvatar({
     super.key,
-    required this.photoPath,
+    this.photoPath,
+    this.photoBase64,
     required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
+    ImageProvider? imageProvider;
+
+    // PRIORITAS 1: base64 dari firestore
+    if (photoBase64 != null && photoBase64!.isNotEmpty) {
+      try {
+        imageProvider = MemoryImage(base64Decode(photoBase64!));
+      } catch (e) {
+        debugPrint("Error decode base64: $e");
+      }
+    }
+
+    // PRIORITAS 2: file lokal
+    else if (photoPath != null && photoPath!.isNotEmpty) {
+      imageProvider = FileImage(File(photoPath!));
+    }
+
     return GestureDetector(
       onTap: onTap,
       child: Stack(
@@ -20,15 +39,12 @@ class ProfileAvatar extends StatelessWidget {
           CircleAvatar(
             radius: 55,
             backgroundColor: Colors.blue.shade100,
-            backgroundImage: photoPath != null && photoPath!.isNotEmpty
-                ? FileImage(File(photoPath!))
-                : null,
-            child: photoPath == null
+            backgroundImage: imageProvider,
+            child: imageProvider == null
                 ? const Icon(Icons.person, size: 60, color: Colors.blue)
                 : null,
           ),
 
-          // icon kamera kecil
           Positioned(
             bottom: 0,
             right: 0,
