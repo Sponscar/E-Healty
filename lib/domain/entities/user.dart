faf021ca993d4d62eb5b331d553e5ed0e690d166
Sponscar <panji.sabilillah23@student.uisi.ac.id>
2026-01-18 02:48:42 +0700

    Update user entity

--- lib/domain/entities/user.dart
+++ lib/domain/entities/user.dart
@@ -4,6 +4,8 @@ class UserEntity {
   final String email;
   final String phoneNumber;
   final String role;
+  final String? photoPath;
+  final String? photoBase64;
 
   UserEntity({
     required this.uid,
@@ -11,5 +13,25 @@ class UserEntity {
     required this.email,
     required this.phoneNumber,
     required this.role,
+    this.photoPath,
+    this.photoBase64,
   });
+
+  // helper copyWith (biar gampang update sebagian data)
+  UserEntity copyWith({
+    String? name,
+    String? phoneNumber,
+    String? photoPath,
+    String? photoBase64,
+  }) {
+    return UserEntity(
+      uid: uid,
+      name: name ?? this.name,
+      email: email,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      role: role,
+      photoPath: photoPath ?? this.photoPath,
+      photoBase64: photoBase64 ?? this.photoBase64,
+    );
+  }
 }
